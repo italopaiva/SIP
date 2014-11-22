762fefc3e819bc059364827100cd99aae0a2de53
italopaiva <italosensei@hotmail.com>
2014-11-22 01:21:03 -0200

    Fixing database data

--- siga.sql
+++ siga.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.10
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 21-Nov-2014 às 18:59
--- Versão do servidor: 5.6.20
--- PHP Version: 5.4.24
+-- Generation Time: Nov 22, 2014 at 01:17 AM
+-- Server version: 5.5.40-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,44 +19,35 @@ SET time_zone = "+00:00";
 --
 -- Database: `siga`
 --
-CREATE DATABASE IF NOT EXISTS `siga` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `siga`;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `course`
+-- Table structure for table `course`
 --
 
 CREATE TABLE IF NOT EXISTS `course` (
-`id` int(11) NOT NULL,
-  `course_name` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Truncate table before insert `course`
---
-
-TRUNCATE TABLE `course`;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `course_type`
+-- Table structure for table `course_type`
 --
 
 CREATE TABLE IF NOT EXISTS `course_type` (
-`id_course_type` int(11) NOT NULL,
+  `id_course_type` int(11) NOT NULL AUTO_INCREMENT,
   `name_course_type` varchar(40) CHARACTER SET latin1 NOT NULL,
-  `financiated` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `financiated` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_course_type`),
+  UNIQUE KEY `name_course_type` (`name_course_type`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
--- Truncate table before insert `course_type`
---
-
-TRUNCATE TABLE `course_type`;
---
--- Extraindo dados da tabela `course_type`
+-- Dumping data for table `course_type`
 --
 
 INSERT INTO `course_type` (`id_course_type`, `name_course_type`, `financiated`) VALUES
@@ -67,37 +58,29 @@ INSERT INTO `course_type` (`id_course_type`, `name_course_type`, `financiated`) 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `departamentos`
+-- Table structure for table `departamentos`
 --
 
 CREATE TABLE IF NOT EXISTS `departamentos` (
-`id` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Truncate table before insert `departamentos`
---
-
-TRUNCATE TABLE `departamentos`;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `funcionarios`
+-- Table structure for table `funcionarios`
 --
 
 CREATE TABLE IF NOT EXISTS `funcionarios` (
-`id` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
--- Truncate table before insert `funcionarios`
---
-
-TRUNCATE TABLE `funcionarios`;
---
--- Extraindo dados da tabela `funcionarios`
+-- Dumping data for table `funcionarios`
 --
 
 INSERT INTO `funcionarios` (`id`, `nome`) VALUES
@@ -107,21 +90,17 @@ INSERT INTO `funcionarios` (`id`, `nome`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `funcoes`
+-- Table structure for table `funcoes`
 --
 
 CREATE TABLE IF NOT EXISTS `funcoes` (
-`id` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- Truncate table before insert `funcoes`
---
-
-TRUNCATE TABLE `funcoes`;
---
--- Extraindo dados da tabela `funcoes`
+-- Dumping data for table `funcoes`
 --
 
 INSERT INTO `funcoes` (`id`, `nome`) VALUES
@@ -130,22 +109,18 @@ INSERT INTO `funcoes` (`id`, `nome`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `graduation`
+-- Table structure for table `graduation`
 --
 
 CREATE TABLE IF NOT EXISTS `graduation` (
-`id_graduation` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id_graduation` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_graduation`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Truncate table before insert `graduation`
---
-
-TRUNCATE TABLE `graduation`;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `migrations`
+-- Table structure for table `migrations`
 --
 
 CREATE TABLE IF NOT EXISTS `migrations` (
@@ -153,12 +128,7 @@ CREATE TABLE IF NOT EXISTS `migrations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Truncate table before insert `migrations`
---
-
-TRUNCATE TABLE `migrations`;
---
--- Extraindo dados da tabela `migrations`
+-- Dumping data for table `migrations`
 --
 
 INSERT INTO `migrations` (`version`) VALUES
@@ -167,21 +137,17 @@ INSERT INTO `migrations` (`version`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `module`
+-- Table structure for table `module`
 --
 
 CREATE TABLE IF NOT EXISTS `module` (
-`id_module` int(11) NOT NULL,
-  `module_name` varchar(20) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `id_module` int(11) NOT NULL AUTO_INCREMENT,
+  `module_name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_module`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
--- Truncate table before insert `module`
---
-
-TRUNCATE TABLE `module`;
---
--- Extraindo dados da tabela `module`
+-- Dumping data for table `module`
 --
 
 INSERT INTO `module` (`id_module`, `module_name`) VALUES
@@ -191,21 +157,18 @@ INSERT INTO `module` (`id_module`, `module_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `module_permission`
+-- Table structure for table `module_permission`
 --
 
 CREATE TABLE IF NOT EXISTS `module_permission` (
   `id_module` int(11) NOT NULL,
-  `id_permission` int(11) NOT NULL
+  `id_permission` int(11) NOT NULL,
+  KEY `id_module` (`id_module`),
+  KEY `id_permission` (`id_permission`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Truncate table before insert `module_permission`
---
-
-TRUNCATE TABLE `module_permission`;
---
--- Extraindo dados da tabela `module_permission`
+-- Dumping data for table `module_permission`
 --
 
 INSERT INTO `module_permission` (`id_module`, `id_permission`) VALUES
@@ -215,26 +178,23 @@ INSERT INTO `module_permission` (`id_module`, `id_permission`) VALUES
 (2, 7),
 (2, 6),
 (2, 3),
-(2, 1);
+(2, 1),
+(2, 8);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `permission`
+-- Table structure for table `permission`
 --
 
 CREATE TABLE IF NOT EXISTS `permission` (
-`id_permission` int(11) NOT NULL,
-  `permission_name` varchar(20) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+  `id_permission` int(11) NOT NULL AUTO_INCREMENT,
+  `permission_name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_permission`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
 --
--- Truncate table before insert `permission`
---
-
-TRUNCATE TABLE `permission`;
---
--- Extraindo dados da tabela `permission`
+-- Dumping data for table `permission`
 --
 
 INSERT INTO `permission` (`id_permission`, `permission_name`) VALUES
@@ -242,28 +202,26 @@ INSERT INTO `permission` (`id_permission`, `permission_name`) VALUES
 (3, 'funcionarios'),
 (4, 'setores'),
 (6, 'funcoes'),
-(7, 'departamentos');
+(7, 'departamentos'),
+(8, 'cursos');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `postgraduation_type`
+-- Table structure for table `postgraduation_type`
 --
 
 CREATE TABLE IF NOT EXISTS `postgraduation_type` (
-`id_postgraduation_type` int(11) NOT NULL,
+  `id_postgraduation_type` int(11) NOT NULL AUTO_INCREMENT,
   `name_postgraduation_type` varchar(40) CHARACTER SET ucs2 NOT NULL,
   `id_course_type` int(11) NOT NULL,
-  `financiated` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `financiated` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_postgraduation_type`),
+  KEY `id_course_type` (`id_course_type`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
--- Truncate table before insert `postgraduation_type`
---
-
-TRUNCATE TABLE `postgraduation_type`;
---
--- Extraindo dados da tabela `postgraduation_type`
+-- Dumping data for table `postgraduation_type`
 --
 
 INSERT INTO `postgraduation_type` (`id_postgraduation_type`, `name_postgraduation_type`, `id_course_type`, `financiated`) VALUES
@@ -273,21 +231,17 @@ INSERT INTO `postgraduation_type` (`id_postgraduation_type`, `name_postgraduatio
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `setores`
+-- Table structure for table `setores`
 --
 
 CREATE TABLE IF NOT EXISTS `setores` (
-`id` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
--- Truncate table before insert `setores`
---
-
-TRUNCATE TABLE `setores`;
---
--- Extraindo dados da tabela `setores`
+-- Dumping data for table `setores`
 --
 
 INSERT INTO `setores` (`id`, `nome`) VALUES
@@ -297,25 +251,21 @@ INSERT INTO `setores` (`id`, `nome`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(70) NOT NULL,
   `cpf` varchar(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `login` varchar(20) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
 
 --
--- Truncate table before insert `users`
---
-
-TRUNCATE TABLE `users`;
---
--- Extraindo dados da tabela `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `cpf`, `email`, `login`, `password`) VALUES
@@ -331,52 +281,46 @@ INSERT INTO `users` (`id`, `name`, `cpf`, `email`, `login`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `user_module`
+-- Table structure for table `user_module`
 --
 
 CREATE TABLE IF NOT EXISTS `user_module` (
   `id_user` int(11) NOT NULL,
-  `id_module` int(11) NOT NULL
+  `id_module` int(11) NOT NULL,
+  KEY `id_user` (`id_user`),
+  KEY `id_module` (`id_module`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Truncate table before insert `user_module`
---
-
-TRUNCATE TABLE `user_module`;
---
--- Extraindo dados da tabela `user_module`
+-- Dumping data for table `user_module`
 --
 
 INSERT INTO `user_module` (`id_user`, `id_module`) VALUES
-(3, 1),
-(4, 2),
-(5, 1),
-(6, 1),
+(1, 1),
+(1, 2),
+(1, 1),
+(1, 2),
+(2, 1),
 (3, 2);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `user_type`
+-- Table structure for table `user_type`
 --
 
 CREATE TABLE IF NOT EXISTS `user_type` (
-`id_type` int(11) NOT NULL,
-  `type_name` varchar(15) CHARACTER SET latin1 NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+  `id_type` int(11) NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(15) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id_type`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
--- Truncate table before insert `user_type`
---
-
-TRUNCATE TABLE `user_type`;
---
--- Extraindo dados da tabela `user_type`
+-- Dumping data for table `user_type`
 --
 
 INSERT INTO `user_type` (`id_type`, `type_name`) VALUES
-(1, 'admininstrador'),
+(1, 'administrador'),
 (2, 'discente'),
 (3, 'docente'),
 (4, 'secretaria'),
@@ -385,223 +329,54 @@ INSERT INTO `user_type` (`id_type`, `type_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `user_user_type`
+-- Table structure for table `user_user_type`
 --
 
 CREATE TABLE IF NOT EXISTS `user_user_type` (
   `id_user` int(11) NOT NULL,
-  `id_user_type` int(11) NOT NULL
+  `id_user_type` int(11) NOT NULL,
+  KEY `id_user_type` (`id_user_type`),
+  KEY `id_user` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Truncate table before insert `user_user_type`
---
-
-TRUNCATE TABLE `user_user_type`;
---
--- Extraindo dados da tabela `user_user_type`
+-- Dumping data for table `user_user_type`
 --
 
 INSERT INTO `user_user_type` (`id_user`, `id_user_type`) VALUES
-(4, 2),
-(5, 3),
-(6, 4),
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 5),
 (7, 5),
-(3, 1),
 (8, 5),
-(9, 3),
-(10, 4),
-(11, 2),
-(12, 5);
+(2, 3);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `course`
---
-ALTER TABLE `course`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `course_type`
---
-ALTER TABLE `course_type`
- ADD PRIMARY KEY (`id_course_type`), ADD UNIQUE KEY `name_course_type` (`name_course_type`);
-
---
--- Indexes for table `departamentos`
---
-ALTER TABLE `departamentos`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `funcionarios`
---
-ALTER TABLE `funcionarios`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `funcoes`
---
-ALTER TABLE `funcoes`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `graduation`
---
-ALTER TABLE `graduation`
- ADD PRIMARY KEY (`id_graduation`);
-
---
--- Indexes for table `module`
---
-ALTER TABLE `module`
- ADD PRIMARY KEY (`id_module`);
-
---
--- Indexes for table `module_permission`
---
-ALTER TABLE `module_permission`
- ADD KEY `id_module` (`id_module`), ADD KEY `id_permission` (`id_permission`);
-
---
--- Indexes for table `permission`
---
-ALTER TABLE `permission`
- ADD PRIMARY KEY (`id_permission`);
-
---
--- Indexes for table `postgraduation_type`
---
-ALTER TABLE `postgraduation_type`
- ADD PRIMARY KEY (`id_postgraduation_type`), ADD KEY `id_course_type` (`id_course_type`);
-
---
--- Indexes for table `setores`
---
-ALTER TABLE `setores`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
- ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user_module`
---
-ALTER TABLE `user_module`
- ADD KEY `id_user` (`id_user`), ADD KEY `id_module` (`id_module`);
-
---
--- Indexes for table `user_type`
---
-ALTER TABLE `user_type`
- ADD PRIMARY KEY (`id_type`);
-
---
--- Indexes for table `user_user_type`
---
-ALTER TABLE `user_user_type`
- ADD KEY `id_user_type` (`id_user_type`), ADD KEY `id_user` (`id_user`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `course`
---
-ALTER TABLE `course`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `course_type`
---
-ALTER TABLE `course_type`
-MODIFY `id_course_type` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `departamentos`
---
-ALTER TABLE `departamentos`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `funcionarios`
---
-ALTER TABLE `funcionarios`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `funcoes`
---
-ALTER TABLE `funcoes`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `graduation`
---
-ALTER TABLE `graduation`
-MODIFY `id_graduation` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `module`
---
-ALTER TABLE `module`
-MODIFY `id_module` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `permission`
---
-ALTER TABLE `permission`
-MODIFY `id_permission` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT for table `postgraduation_type`
---
-ALTER TABLE `postgraduation_type`
-MODIFY `id_postgraduation_type` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `setores`
---
-ALTER TABLE `setores`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
---
--- AUTO_INCREMENT for table `user_type`
---
-ALTER TABLE `user_type`
-MODIFY `id_type` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- Constraints for dumped tables
 --
 
 --
--- Limitadores para a tabela `module_permission`
+-- Constraints for table `module_permission`
 --
 ALTER TABLE `module_permission`
-ADD CONSTRAINT `PERMISSION_ID_FK` FOREIGN KEY (`id_permission`) REFERENCES `permission` (`id_permission`),
-ADD CONSTRAINT `PERMISSION_MODULE_ID_FK` FOREIGN KEY (`id_module`) REFERENCES `module` (`id_module`);
+  ADD CONSTRAINT `PERMISSION_ID_FK` FOREIGN KEY (`id_permission`) REFERENCES `permission` (`id_permission`),
+  ADD CONSTRAINT `PERMISSION_MODULE_ID_FK` FOREIGN KEY (`id_module`) REFERENCES `module` (`id_module`);
 
 --
--- Limitadores para a tabela `postgraduation_type`
+-- Constraints for table `postgraduation_type`
 --
 ALTER TABLE `postgraduation_type`
-ADD CONSTRAINT `postgraduation_type_ibfk_1` FOREIGN KEY (`id_course_type`) REFERENCES `course_type` (`id_course_type`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `postgraduation_type_ibfk_1` FOREIGN KEY (`id_course_type`) REFERENCES `course_type` (`id_course_type`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Limitadores para a tabela `user_module`
+-- Constraints for table `user_module`
 --
 ALTER TABLE `user_module`
-ADD CONSTRAINT `MODULE_ID_FK` FOREIGN KEY (`id_module`) REFERENCES `module` (`id_module`),
-ADD CONSTRAINT `MODULE_USER_ID_FK` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
-
---
--- Limitadores para a tabela `user_user_type`
---
-ALTER TABLE `user_user_type`
-ADD CONSTRAINT `USER_ID_FK` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
-ADD CONSTRAINT `USER_TYPE_FK` FOREIGN KEY (`id_user_type`) REFERENCES `user_type` (`id_type`);
+  ADD CONSTRAINT `MODULE_ID_FK` FOREIGN KEY (`id_module`) REFERENCES `module` (`id_module`),
+  ADD CONSTRAINT `MODULE_USER_ID_FK` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
